7bb61df55671108e3f31aeafc98e83450e112a17
mohamoha0000 <moham3iof@gmail.com>
2025-04-24 18:06:06 +0100

    v4

---
+++ Soli-Hackathon-2025/assets/home.js
@@ -0,0 +1,5 @@
+
+// Mobile Menu Toggle
+document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('nav-active');
+});
--- Soli-Hackathon-2025/assets/script.js
+++
@@ -1,5 +0,0 @@
-
-// Mobile Menu Toggle
-document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('nav-active');
-});
